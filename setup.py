256975d8935cb40db302f2c86005baebdcc1d4c7
abhishekkrthakur <abhishek4@gmail.com>
2019-12-14 10:25:16 +0100

    add long desc

--- setup.py
+++ setup.py
@@ -3,11 +3,17 @@ from setuptools import find_packages
 
 import sandesh
 
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+
 if __name__ == "__main__":
     setup(
         name="sandesh",
         version=sandesh.__version__,
         description="sandesh - a simple app to send messages on slack",
+        long_description=long_description,
+        long_description_content_type='text/markdown',
         author="Abhishek Thakur",
         author_email="abhishek4@gmail.com",
         url="https://github.com/abhishekkrthakur/sandesh",
