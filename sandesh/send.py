acf53be17c3ca5482541209b68a28fbbbdac2cdd
abhishek thakur <abhishek4@gmail.com>
2022-12-17 09:32:59 +0000

    update version

--- sandesh/send.py
+++ sandesh/send.py
@@ -5,9 +5,29 @@ import collections
 import json
 import os
 import requests
+from loguru import logger
 
 
-def send(msg, use_raw=False, webhook=None):
+def http_post(url: str, timeout: int = 5, payload=None) -> requests.Response:
+    try:
+        response = requests.post(url=url, data=payload, timeout=timeout)
+    except requests.exceptions.ConnectionError:
+        logger.error("❌ Failed to reach slack webhook. Check your internet connection")
+    except requests.exceptions.HTTPError:
+        logger.error("❌ Slack webhook returned an error")
+    except requests.exceptions.Timeout:
+        logger.error("❌ Slack webhook timed out")
+    except requests.exceptions.RequestException:
+        logger.error("❌ Slack webhook returned an error")
+    except Exception as e:
+        logger.error(f"❌ Slack webhook error: {e}")
+    if response.status_code != 200:
+        logger.error(f"❌ Slack webhook returned status code: {response.status_code}")
+    else:
+        logger.info("✅ Message sent to slack")
+
+
+def send(msg, timeout=5, use_raw=False, webhook=None):
     """
     A function to send messages on slack
     :param msg: string, list of strings, dictionary (or defaultdict/OrderedDict)
@@ -16,29 +36,40 @@ def send(msg, use_raw=False, webhook=None):
     """
     if webhook is None:
         webhook = os.environ.get("SANDESH_WEBHOOK")
-    
+
     if webhook is None:
-        raise Exception("No webhooks found")
-    
+        logger.error("Sandesh: No webhooks found")
+        return
+
     data = dict()
-    
+
     if isinstance(msg, str):
-        data["text"] = msg
-        requests.post(webhook, json.dumps(data))
-    
+        data = {
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": msg,
+                    },
+                }
+            ]
+        }
+        http_post(webhook, payload=json.dumps(data), timeout=timeout)
+
     elif isinstance(msg, list):
-        data["text"] = '\n'.join(msg)
-        requests.post(webhook, json.dumps(data))
-    
+        data["text"] = "\n".join(msg)
+        http_post(webhook, payload=json.dumps(data), timeout=timeout)
+
     elif isinstance(msg, (dict, collections.defaultdict, collections.OrderedDict)):
         values = []
         for k, v in msg.items():
             values.append(str(k) + ": " + str(v))
-        data["text"] = '\n'.join(values)
-        requests.post(webhook, json.dumps(data))
-    
+        data["text"] = "\n".join(values)
+        http_post(webhook, payload=json.dumps(data), timeout=timeout)
+
     elif isinstance(msg, dict) and use_raw is True:
-        requests.post(webhook, json.dumps(msg))
-    
+        http_post(webhook, payload=json.dumps(msg), timeout=timeout)
+
     else:
-        raise Exception("Type for variable: msg, is currently not supported")
+        logger.error("Sandesh: Type for variable `msg`, is currently not supported")
--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ from setuptools import find_packages
 
 import sandesh
 
-with open('README.md', encoding='utf-8') as f:
+with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
 
+INSTALL_REQUIRES = [
+    "loguru",
+]
 
 if __name__ == "__main__":
     setup(
@@ -13,13 +16,13 @@ if __name__ == "__main__":
         version=sandesh.__version__,
         description="sandesh - a simple app to send messages on slack",
         long_description=long_description,
-        long_description_content_type='text/markdown',
+        long_description_content_type="text/markdown",
         author="Abhishek Thakur",
-        author_email="abhishek4@gmail.com",
         url="https://github.com/abhishekkrthakur/sandesh",
         license="MIT License",
         packages=find_packages(),
         include_package_data=True,
         platforms=["linux", "unix"],
-        python_requires='>3.5.2'
+        python_requires=">=3.7",
+        install_requires=INSTALL_REQUIRES,
     )
